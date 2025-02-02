1d82172911815a819c6e3c40440a1bd932a91f0b
Allen <124698362+Maleleee@users.noreply.github.com>
2025-02-02 19:33:12 +0800

    Update oauthgit.py

--- oauthgit.py
+++ oauthgit.py
@@ -2,11 +2,11 @@ import requests
 from flask import Flask, redirect, url_for, session, request
 
 app = Flask(__name__)
-app.secret_key = '941bde3c8ae1aabd8006045249b0a1bfabaffc20'
+app.secret_key = ##
 
 # OAuth configuration for GitHub
-CLIENT_ID = 'Ov23li8BNyKIHZmGnlUm'
-CLIENT_SECRET = '941bde3c8ae1aabd8006045249b0a1bfabaffc20'
+CLIENT_ID = ##
+CLIENT_SECRET = ##
 REDIRECT_URI = 'http://localhost:5000/oauth_callback'
 AUTHORIZE_URL = 'https://github.com/login/oauth/authorize'
 TOKEN_URL = 'https://github.com/login/oauth/access_token'
